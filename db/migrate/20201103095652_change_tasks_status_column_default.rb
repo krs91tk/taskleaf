79744cbdbe90331ddb58871f784b6c6b107627ab
krs91tk <krs91tk@gmail.com>
2020-11-03 19:38:43 +0900

    change status column of tasks

--- db/migrate/20201103095652_change_tasks_status_column_default.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTasksStatusColumnDefault < ActiveRecord::Migration[5.2]
-  def up
-    change_column :tasks, :status,:integer, default: 0, after: :priority
-  end
-
-  def down
-    change_column :tasks, :status,:integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_095652) do
+ActiveRecord::Schema.define(version: 2020_11_03_103438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 2020_11_03_095652) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.integer "status", default: 0
     t.datetime "deadline", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.integer "priority", default: 1, null: false
+    t.integer "status", default: 0, null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
